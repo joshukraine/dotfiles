67c511b9a019f5ae74e7c3919271222820da6a2a
Joshua Steele <joshukraine@gmail.com>
2015-03-27 23:20:05 +0200

    Add git config lines from setup.sh.

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -60,6 +60,23 @@ echo ""
 ####################################
 
 echo ""
+echo "Configuring git..."
+echo ""
+
+git config --global user.name "Joshua Steele"
+git config --global user.email joshukraine@gmail.com
+git config --global core.editor vim
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.sta status
+git config --global alias.st "status --short"
+git config --global alias.df diff
+git config --global alias.dfs "diff --staged"
+git config --global alias.logg "log --graph --decorate --oneline --abbrev-commit --all"
+git config --global core.excludesfile $HOME/.gitignore_global
+
+echo ""
 echo "Installing Vundle..."
 echo ""
 if [ -d $HOME/.vim/bundle ]; then
