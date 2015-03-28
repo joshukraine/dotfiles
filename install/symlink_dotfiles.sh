f7fc899e976c0c73e9bdc97c86d8a34321725473
Joshua Steele <joshukraine@gmail.com>
2015-03-28 16:36:09 +0200

    Rename setup_dotfiles.sh to symlink_dotfiles.sh, move to install directory.

--- install/symlink_dotfiles.sh
+++ install/symlink_dotfiles.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ####################################
-# setup_dotfiles.sh
+# symlink_dotfiles.sh
 #
 # This script backs up any old
 # dotfiles on the system, and
@@ -46,7 +46,7 @@ echo ""
 
 for file in $files; do
   echo ""
-  echo "Linking $DOTFILES/$file to $HOME/.$file..."
+  echo "-> Linking $DOTFILES/$file to $HOME/.$file..."
   echo ""
   ln -nfs "$DOTFILES/$file" "$HOME/.$file"
 done
@@ -54,35 +54,3 @@ done
 echo ""
 echo "Symlinks complete!"
 echo ""
-
-####################################
-# Misc tasks
-####################################
-
-echo ""
-echo "Configuring git..."
-echo ""
-
-git config --global user.name "Joshua Steele"
-git config --global user.email joshukraine@gmail.com
-git config --global core.editor vim
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.sta status
-git config --global alias.st "status --short"
-git config --global alias.df diff
-git config --global alias.dfs "diff --staged"
-git config --global alias.logg "log --graph --decorate --oneline --abbrev-commit --all"
-git config --global core.excludesfile $HOME/.gitignore_global
-
-echo ""
-echo "Installing Vundle..."
-echo ""
-if [ -d $HOME/.vim/bundle ]; then
-  rm -rf $HOME/.vim/bundle
-fi
-git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-cp -R $DOTFILES/vim/colors $HOME/.vim # So vim won't complain about solarized not being found.
-vim +PluginInstall +qall
-rm -rf $HOME/.vim/colors
