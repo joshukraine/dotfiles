92692a53f4daaa7cf00526d0de2892be144f0708
Joshua Steele <joshukraine@gmail.com>
2015-03-30 10:27:33 +0300

    Comment cleanup. Add 'done' lines at the end of each operation.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-####################################
+################################################################################
 # setup.sh
 #
-# This script is intended to set up
-# a new Mac computer with my
-# dotfiles and other development
-# preferences.
-####################################
+# This script is intended to set up a new Mac computer with my dotfiles and
+# other development preferences.
+################################################################################
+
 
 set -e # Terminate script if anything exits with a non-zero value
 set -u # Prevent unset variables
@@ -19,19 +18,18 @@ fancy_echo() { # Thank you, thoughtbot. :)
   printf "\n$fmt\n" "$@"
 }
 
-####################################
+################################################################################
 # Set some variables
-####################################
+################################################################################
 
 osname=$(uname)
 divider="====>"
 COMMANDLINE_TOOLS="/Library/Developer/CommandLineTools"
 DOTFILES_DIR=$HOME/dotfiles
 
-####################################
-# Check for presence of command line
-# tools if OS X
-####################################
+################################################################################
+# Check for presence of command line tools if OS X
+################################################################################
 
 if [ "$osname" == 'Darwin' && ! -d "$COMMANDLINE_TOOLS" ]; then
   fancy_echo "Apple's command line developer tools must be installed before running this script. To install them, just run 'gcc' from the terminal and then follow the prompts. Once the command line tools have been installed, you can try running this script again."
@@ -47,20 +45,17 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-
-
-
-####################################
+################################################################################
 # Install oh-my-zsh
-####################################
+################################################################################
 
 fancy_echo "$divider Step 1: Installing oh-my-zsh..."
 
 curl -L http://install.ohmyz.sh | sh
 
-####################################
+################################################################################
 # Provision with Laptop if OS X
-####################################
+################################################################################
 
 fancy_echo "$divider Step 2: Invoking Laptop script from thoughtbot..."
 
@@ -72,9 +67,9 @@ elif [ "$osname" == 'Linux' ]; then
   fancy_echo "OK, we're on Linux, which is not supported by Laptop. Skipping..."
 fi
 
-####################################
+################################################################################
 # Setup basic directories
-####################################
+################################################################################
 
 fancy_echo "$divider Step 3: Creating a few basic directories..."
 
@@ -85,82 +80,80 @@ mkdir -p $HOME/Developer/cheatsheets
 mkdir -p $HOME/Developer/other_code
 mkdir -p $HOME/src
 
-####################################
+################################################################################
 # Setup dotfiles
-####################################
+################################################################################
 
 fancy_echo "$divider Step 4: Installing dotfiles..."
 
 cd $HOME
-
 if [ ! -d $DOTFILES_DIR ]; then
   git clone https://github.com/joshukraine/dotfiles.git $DOTFILES_DIR
 else
   cd $DOTFILES_DIR
   git pull origin master
 fi
-
+fancy_echo "Starting dotfiles setup script..."
 source "$DOTFILES_DIR/install/symlink_dotfiles.sh"
-
 fancy_echo "Dotfiles setup complete! Resuming main setup script..."
 
-####################################
+################################################################################
 # Install Powerline-patched fonts
-####################################
+################################################################################
 
 fancy_echo "$divider Step 5: Installing fixed-width fonts patched for use with Powerline symbols..."
-
 if [ -d "$HOME/src/fonts" ]; then
   mv $HOME/src/fonts $HOME/src/fonts_backup
 fi
-
 git clone https://github.com/powerline/fonts.git $HOME/src/fonts
 cd $HOME/src/fonts
 ./install.sh
 cd $HOME
 rm -rf $HOME/src/fonts
+fancy_echo "Done!"
 
-####################################
+################################################################################
 # Configure git
-####################################
+################################################################################
 
 fancy_echo "$divider Step 6: Configuring git..."
-
 source "$DOTFILES_DIR/install/git-setup.sh"
+fancy_echo "Done!"
 
-####################################
+################################################################################
 # Install Vundle and vim plugins
-####################################
+################################################################################
 
 fancy_echo "$divider Step 7: Installing Vundle and vim plugins..."
-
 source "$DOTFILES_DIR/install/vundle.sh"
+fancy_echo "Done!"
 
-####################################
+################################################################################
 # Install extra Homebrew packages
-####################################
+################################################################################
 
 fancy_echo "$divider Step 8: Installing extra Homebrew formulae..."
-
 source "$DOTFILES_DIR/install/brew.sh"
+fancy_echo "Done!"
 
-####################################
+################################################################################
 # Install Cask and related software
-####################################
+################################################################################
 
 fancy_echo "$divider Step 9: Installing Cask and related software..."
-
 brew install caskroom/cask/brew-cask
 source "$DOTFILES_DIR/install/brew-cask.sh"
+fancy_echo "Done!"
 
-####################################
+################################################################################
 # Set OS X preferences
-####################################
+################################################################################
 
 if [ "$osname" == 'Darwin' ]; then
   fancy_echo "$divider Step 10: Setting OS X preferences..."
   source "$DOTFILES_DIR/osx/defaults.sh"
   source "$DOTFILES_DIR/osx/dock.sh"
+  fancy_echo "Bam! OS X prefs set successfully. Some changes may require a restart to take effect."
 fi
 
 fancy_echo "Setup complete! Please restart your terminal."
