33efcbb3f2bd726b26f2b56f5b8cdabd9d33891c
Joshua Steele <joshukraine@gmail.com>
2015-03-30 22:19:44 +0300

    Add some extra thoughtbot functions for Homebrew.

--- setup.sh
+++ setup.sh
@@ -7,20 +7,72 @@
 # other development preferences.
 ################################################################################
 
+################################################################################
+# First, some helpful functions borrowed from Laptop. Thank you, thoughtbot. :)
+################################################################################
 
-set -e # Terminate script if anything exits with a non-zero value
-set -u # Prevent unset variables
-
-fancy_echo() { # Thank you, thoughtbot. :)
+fancy_echo() {
   local fmt="$1"; shift
 
   # shellcheck disable=SC2059
   printf "\n$fmt\n" "$@"
 }
 
+brew_install_or_upgrade() {
+  if brew_is_installed "$1"; then
+    if brew_is_upgradable "$1"; then
+      fancy_echo "Upgrading %s ..." "$1"
+      brew upgrade "$@"
+    else
+      fancy_echo "Already using the latest version of %s. Skipping ..." "$1"
+    fi
+  else
+    fancy_echo "Installing %s ..." "$1"
+    brew install "$@"
+  fi
+}
+
+brew_is_installed() {
+  local name="$(brew_expand_alias "$1")"
+
+  brew list -1 | grep -Fqx "$name"
+}
+
+brew_is_upgradable() {
+  local name="$(brew_expand_alias "$1")"
+
+  ! brew outdated --quiet "$name" >/dev/null
+}
+
+brew_tap() {
+  brew tap "$1" 2> /dev/null
+}
+
+brew_expand_alias() {
+  brew info "$1" 2>/dev/null | head -1 | awk '{gsub(/:/, ""); print $1}'
+}
+
+brew_launchctl_restart() {
+  local name="$(brew_expand_alias "$1")"
+  local domain="homebrew.mxcl.$name"
+  local plist="$domain.plist"
+
+  fancy_echo "Restarting %s ..." "$1"
+  mkdir -p "$HOME/Library/LaunchAgents"
+  ln -sfv "/usr/local/opt/$name/$plist" "$HOME/Library/LaunchAgents"
+
+  if launchctl list | grep -Fq "$domain"; then
+    launchctl unload "$HOME/Library/LaunchAgents/$plist" >/dev/null
+  fi
+  launchctl load "$HOME/Library/LaunchAgents/$plist" >/dev/null
+}
+
 ################################################################################
-# Set some variables
+# Next, a little more setup...
 ################################################################################
+
+set -e # Terminate script if anything exits with a non-zero value
+set -u # Prevent unset variables
 
 osname=$(uname)
 divider="====>"
@@ -69,6 +121,7 @@ mkdir -p $HOME/Developer/training
 mkdir -p $HOME/Developer/vms
 mkdir -p $HOME/Developer/cheatsheets
 mkdir -p $HOME/Developer/other_code
+mkdir -p $HOME/Developer/fav_repos
 mkdir -p $HOME/src
 
 ################################################################################
@@ -132,7 +185,7 @@ fancy_echo "Done!"
 ################################################################################
 
 fancy_echo "$divider Step 9: Installing Cask and related software..."
-brew install caskroom/cask/brew-cask
+brew_install_or_upgrade caskroom/cask/brew-cask
 source "$DOTFILES_DIR/install/brew-cask.sh"
 fancy_echo "Done!"
 
