1edaf0a7c357cb81a3cf0ae79097c1b94868faaa
Joshua Steele <joshukraine@gmail.com>
2015-03-30 11:43:20 +0300

    Remove call to sudo -v at beginning of script.

--- setup.sh
+++ setup.sh
@@ -36,15 +36,6 @@ if [[ "$osname" == 'Darwin' && ! -d "$COMMANDLINE_TOOLS" ]]; then
   exit 1
 fi
 
-
-
-
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 ################################################################################
 # Install oh-my-zsh
 ################################################################################
