3a92b007d50e126976f639c0a740f1bf1a69308c
Joshua Steele <joshukraine@gmail.com>
2015-04-07 14:03:08 +0300

    Correct dock paths.

--- osx/dock.sh
+++ osx/dock.sh
@@ -2,16 +2,16 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Launchpad.app"
-dockutil --no-restart --add "~/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Fantastical 2.app"
 dockutil --no-restart --add "/Applications/Messages.app"
-dockutil --no-restart --add "~/Applications/Skype.app"
+dockutil --no-restart --add "/Applications/Skype.app"
 dockutil --no-restart --add "/Applications/HipChat.app"
-dockutil --no-restart --add "~/Applications/Things.app"
-dockutil --no-restart --add "~/Applications/1Password 5.app"
+dockutil --no-restart --add "/Applications/Things.app"
+dockutil --no-restart --add "/Applications/1Password 5.app"
 dockutil --no-restart --add "/Applications/iTunes.app"
-dockutil --no-restart --add "~/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 
 killall Dock
