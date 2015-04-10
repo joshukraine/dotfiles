281de778c3629c1b2abef4847c487add7dc23fbe
Joshua Steele <joshukraine@gmail.com>
2015-04-10 16:52:19 +0300

    Correct unbound variable error.

--- install/vundle.sh
+++ install/vundle.sh
@@ -10,6 +10,6 @@ if [ -d $HOME/.vim/bundle ]; then
   rm -rf $HOME/.vim/bundle
 fi
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-cp -R $DOTFILES/vim/colors $HOME/.vim # So vim won't complain about solarized not being found.
+cp -R $DOTFILES_DIR/vim/colors $HOME/.vim # So vim won't complain about solarized not being found.
 vim +PluginInstall +qall
 rm -rf $HOME/.vim/colors
