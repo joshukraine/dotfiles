30a203d98e3d713658917157fbc93d062139cc30
Joshua Steele <joshukraine@gmail.com>
2015-09-27 16:25:19 +0300

    Update homebrew install list.

--- install/brew.sh
+++ install/brew.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 
+# In addition to the formulae installed in this script, my fork of thoughtbot's
+# Laptop script first installs the following:
+#
+# zsh
+# git
+# postgres
+# redis
+# the_silver_searcher
+# vim
+# ctags
+# tmux
+# reattach-to-user-namespace
+# imagemagick
+# qt
+# hub
+# node
+# rbenv
+# ruby-build
+# openssl
+# libyaml
+# heroku-toolbelt
+
 brew update
 
 brew_tap 'Goles/battery'
 
 brew_install_or_upgrade 'ansible'
+brew_install_or_upgrade 'awscli'
 brew_install_or_upgrade 'bash-completion'
 brew_install_or_upgrade 'battery'
 brew_install_or_upgrade 'dockutil'
@@ -13,5 +36,6 @@ brew_install_or_upgrade 'htop-osx'
 brew_install_or_upgrade 'namebench'
 brew_install_or_upgrade 'python'
 brew_install_or_upgrade 'ssh-copy-id'
+brew_install_or_upgrade 'trash'
 brew_install_or_upgrade 'tree'
 brew_install_or_upgrade 'wget'
