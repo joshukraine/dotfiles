3eb9fb7fe90adde31289fcc31695cf75dd4dc8ba
Joshua Steele <joshukraine@gmail.com>
2015-10-25 23:18:40 +0200

    Rename 'install' to 'install.sh'. Also change file permissions from 0755 to 0644.

--- install.sh
+++ install.sh
@@ -18,11 +18,10 @@ fancy_echo() {
 set -e # Terminate script if anything exits with a non-zero value
 set -u # Prevent unset variables
 
-divider="====> "
 files="gemrc gitconfig gitignore_global hushlogin pryrc tmux.conf vimrc zshrc"
 DOTFILES_DIR=$HOME/dotfiles
 
-fancy_echo "$divider Installing dotfiles..."
+fancy_echo "Installing dotfiles..."
 
 for file in $files; do
   if [ -f $HOME/.$file ]; then
