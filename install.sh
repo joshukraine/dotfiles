072a5b23d62df1117a6d24b2bba2ac5e1ceb216b
Joshua Steele <joshukraine@gmail.com>
2017-02-11 12:14:33 +0200

    Shellcheck fix: quote variables.

--- install.sh
+++ install.sh
@@ -6,14 +6,12 @@
 # This script symlinks the dotfiles into place in the home directory.
 ################################################################################
 
-
 dotfiles_echo() {
   local fmt="$1"; shift
 
   # shellcheck disable=SC2059
   printf "\n[DOTFILES] $fmt\n" "$@"
 }
-
 
 set -e # Terminate script if anything exits with a non-zero value
 set -u # Prevent unset variables
@@ -24,10 +22,10 @@ DOTFILES_DIR=$HOME/dotfiles
 dotfiles_echo "Installing dotfiles..."
 
 for file in $files; do
-  if [ -f $HOME/.$file ]; then
+  if [ -f "$HOME/.$file" ]; then
     dotfiles_echo ".$file already present. Backing up..."
-    cp $HOME/.$file "$HOME/.${file}_backup"
-    rm -f $HOME/.$file
+    cp "$HOME/.$file" "$HOME/.${file}_backup"
+    rm -f "$HOME/.$file"
   fi
   dotfiles_echo "-> Linking $DOTFILES_DIR/$file to $HOME/.$file..."
   ln -nfs "$DOTFILES_DIR/$file" "$HOME/.$file"
