97b16b752366d93b2ce2e508adebbb83110d8639
Joshua Steele <joshukraine@gmail.com>
2018-02-24 16:32:49 +0200

    Update install.sh for Neovim.
    * Now accommodates both Vim and Neovim.

--- install.sh
+++ install.sh
@@ -16,7 +16,8 @@ dotfiles_echo() {
 set -e # Terminate script if anything exits with a non-zero value
 
 DOTFILES_DIR=$HOME/dotfiles
-VIM_DIR=$DOTFILES_DIR/vim
+VIM_DIR=$HOME/.vim
+NVIM_DIR=$HOME/.config/nvim
 
 files=(
 "asdfrc"
@@ -51,20 +52,31 @@ done
 dotfiles_echo "-> Linking $DOTFILES_DIR/Brewfile to $HOME/Brewfile..."
 ln -nfs "$DOTFILES_DIR"/Brewfile "$HOME"/Brewfile
 
-dotfiles_echo "-> Linking $VIM_DIR/ftplugin to $HOME/.vim/ftplugin..."
+dotfiles_echo "Setting up Vim and Neovim..."
 
-if [ ! -d "$HOME"/.vim/ ]; then
-  mkdir "$HOME"/.vim
+if [ ! -d "$VIM_DIR" ]; then
+  mkdir -p "$VIM_DIR"
 fi
 
-if [ -d "$HOME"/.vim/ftplugin ]; then
-  rm -rf "$HOME"/.vim/ftplugin
+if [ ! -d "$NVIM_DIR" ]; then
+  mkdir -p "$NVIM_DIR"
 fi
 
-ln -nfs "$VIM_DIR"/ftplugin "$HOME"/.vim/ftplugin
+dotfiles_echo "-> Linking $DOTFILES_DIR/vim/ftplugin to $VIM_DIR/ftplugin..."
+ln -nfs "$DOTFILES_DIR"/vim/ftplugin "$VIM_DIR"/ftplugin
 
-dotfiles_echo "-> Linking $VIM_DIR/spell to $HOME/.vim/spell..."
-ln -nfs "$VIM_DIR"/spell "$HOME"/.vim/spell
+dotfiles_echo "-> Linking $DOTFILES_DIR/vim/colors to $VIM_DIR/colors..."
+ln -nfs "$DOTFILES_DIR"/vim/colors "$VIM_DIR"/colors
+
+dotfiles_echo "-> Linking $DOTFILES_DIR/vim/spell to $VIM_DIR/spell..."
+ln -nfs "$DOTFILES_DIR"/vim/spell "$VIM_DIR"/spell
+
+dotfiles_echo "-> Linking $DOTFILES_DIR/init.vim to $NVIM_DIR/init.vim..."
+ln -nfs "$DOTFILES_DIR"/init.vim "$NVIM_DIR"/init.vim
 
 dotfiles_echo "Dotfiles installation complete!"
-dotfiles_echo "Complete Brew Bundle installation with 'brew bundle install'"
+
+dotfiles_echo "Post-install recommendations:"
+dotfiles_echo "- Complete Brew Bundle installation with 'brew bundle install'"
+dotfiles_echo "- The first time you launch Vim or Neovim, plugins will be installed automatically."
+dotfiles_echo "- After launching Neovim, run :checkhealth and resolve any errors/warnings."
