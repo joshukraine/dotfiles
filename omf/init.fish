3487aae26d0bc6e870c021a36ea7504ada80e23a
Joshua Steele <joshukraine@gmail.com>
2019-11-29 22:34:50 +0200

    Add aliases.

--- omf/init.fish
+++ omf/init.fish
@@ -18,4 +18,7 @@ set -xg XDG_CONFIG_HOME "$HOME/.config"
 set -xg XDG_DATA_HOME "$HOME/.local/share"
 set -xg XDG_CACHE_HOME "$HOME/.cache"
 set -xg TERM "screen-256color"
+
+. $OMF_CONFIG/aliases.fish
+
 fish_vi_key_bindings
