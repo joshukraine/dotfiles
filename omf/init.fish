632286e65689b99b6ff81e46fad72b386cae6d7d
Joshua Steele <joshukraine@gmail.com>
2019-11-29 22:30:56 +0200

    Set $PATH.
    * https://fishshell.com/docs/current/tutorial.html#tut_path
    * https://github.com/fish-shell/fish-shell/issues/527

--- omf/init.fish
+++ omf/init.fish
@@ -1,1 +1,10 @@
+# PATH - https://fishshell.com/docs/current/tutorial.html#tut_path
+set -g fish_user_paths "/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/sbin" $fish_user_paths
+set -g fish_user_paths "/bin" $fish_user_paths
+set -g fish_user_paths "/usr/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/bin" $fish_user_paths
+set -g fish_user_paths "$HOME/bin" $fish_user_paths
+
 fish_vi_key_bindings
