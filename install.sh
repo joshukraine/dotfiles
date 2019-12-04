f103be64fbb0dbcd6b2e0b7e33a11f7db93e9e51
Joshua Steele <joshukraine@gmail.com>
2019-12-04 11:45:30 +0200

    Correct script errors.

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ set -e # Terminate script if anything exits with a non-zero value
 
 DOTFILES_DIR=$HOME/dotfiles
 
-home_items=(
+home_files=(
 "asdfrc"
 "default-gems"
 "default-npm-packages"
@@ -35,11 +35,10 @@ home_items=(
 "tool-versions"
 )
 
-config_items=(
+config_dirs=(
 "nvim"
 "omf"
 "ranger"
-"starship.toml"
 )
 
 if [ -z "$XDG_CONFIG_HOME" ]; then
@@ -80,7 +79,7 @@ fi
 
 dotfiles_echo "Installing dotfiles..."
 
-for item in "${home_items[@]}"; do
+for item in "${home_files[@]}"; do
   if [ -f "$HOME"/."$item" ]; then
     dotfiles_echo ".$item already present. Backing up..."
     cp "$HOME"/."$item" "$HOME"/."${item}"_backup
@@ -98,15 +97,18 @@ fi
 dotfiles_echo "-> Linking $DOTFILES_DIR/Brewfile to $HOME/Brewfile..."
 ln -nfs "$DOTFILES_DIR"/Brewfile "$HOME"/Brewfile
 
-for item in "${config_items[@]}"; do
-  if [ -f "$CONFIG_DIR"/"$item" ]; then
+for item in "${config_dirs[@]}"; do
+  if [ -d "$CONFIG_DIR"/"$item" ]; then
     dotfiles_echo "$item already present. Backing up..."
-    cp "$CONFIG_DIR"/"$item" "$CONFIG_DIR"/"${item}"_backup
+    cp -r "$CONFIG_DIR"/"$item" "$CONFIG_DIR"/"${item}"_backup
     rm -rf "$CONFIG_DIR"/"$item"
   fi
-  dotfiles_echo "-> Linking $DOTFILES_DIR/$item to $CONFIG_DIR/.$item..."
+  dotfiles_echo "-> Linking $DOTFILES_DIR/$item to $CONFIG_DIR/$item..."
   ln -nfs "$DOTFILES_DIR"/"$item" "$CONFIG_DIR"/"$item"
 done
+
+dotfiles_echo "-> Linking $DOTFILES_DIR/starship.toml to $CONFIG_DIR/starship.toml..."
+ln -nfs "$DOTFILES_DIR"/starship.toml "$CONFIG_DIR"/starship.toml
 
 dotfiles_echo "Dotfiles installation complete!"
 
