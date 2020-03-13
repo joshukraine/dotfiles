25cd8a98727b18381b3c161c495cb78c6866f701
Joshua Steele <joshukraine@gmail.com>
2020-03-13 10:28:07 +0200

    Be smarter about detecting symlinks vs actual files/directories.

--- install.sh
+++ install.sh
@@ -51,46 +51,61 @@ config_dirs=(
 "ranger"
 )
 
-if [ -z "$XDG_CONFIG_HOME" ]; then
-  if [ ! -d "$HOME/.config" ]; then
-    mkdir "$HOME/.config"
-  fi
-  CONFIG_DIR=$HOME/.config
-else
-  CONFIG_DIR=$XDG_CONFIG_HOME
-fi
-
 dotfiles_echo "Installing dotfiles..."
 
 for item in "${home_files[@]}"; do
-  if [ -f "$HOME"/."$item" ]; then
-    dotfiles_echo ".$item already present. Backing up..."
-    mv -v "$HOME"/."$item" "$HOME"/."${item}_backup"
+  if [ -e "$HOME"/."$item" ]; then
+    dotfiles_echo ".${item} exists."
+    if [ -L "$HOME"/."$item" ]; then
+      dotfiles_echo "Symbolic link detected. Removing..."
+      rm -v "$HOME"/."$item"
+    else
+      dotfiles_echo "Backing up..."
+      mv -v "$HOME"/."$item" "$HOME"/."${item}_backup"
+    fi
   fi
   dotfiles_echo "-> Linking $DOTFILES_DIR/$item to $HOME/.$item..."
   ln -nfs "$DOTFILES_DIR"/"$item" "$HOME"/."$item"
 done
 
-if [ -f "$HOME"/Brewfile ]; then
-  dotfiles_echo "Brewfile already present. Backing up..."
-  mv -v "$HOME"/Brewfile "$HOME"/Brewfile_backup
+if [ -e "$HOME"/Brewfile ]; then
+  dotfiles_echo "Brewfile exists."
+  if [ -L "$HOME"/Brewfile ]; then
+    dotfiles_echo "Symbolic link detected. Removing..."
+    rm -v "$HOME"/Brewfile
+  else
+    dotfiles_echo "Backing up..."
+    mv -v "$HOME"/Brewfile "$HOME"/Brewfile_backup
+  fi
 fi
 dotfiles_echo "-> Linking $DOTFILES_DIR/Brewfile to $HOME/Brewfile..."
 ln -nfs "$DOTFILES_DIR"/Brewfile "$HOME"/Brewfile
 
 for item in "${config_dirs[@]}"; do
   if [ -d "$CONFIG_DIR"/"$item" ]; then
-    if [ -d "$CONFIG_DIR"/"${item}_backup" ]; then
-      rm -rf "$CONFIG_DIR"/"${item}_backup"
+    dotfiles_echo "Directory ${item} exists."
+    if [ -L "$CONFIG_DIR"/"$item" ]; then
+      dotfiles_echo "Symbolic link detected. Removing..."
+      rm -v "$CONFIG_DIR"/"$item"
+    else
+      dotfiles_echo "Backing up..."
+      mv -v "$CONFIG_DIR"/"$item" "$CONFIG_DIR"/"${item}_backup"
     fi
-    dotfiles_echo "$item already present. Backing up..."
-    mv -v "$CONFIG_DIR"/"$item" "$CONFIG_DIR"/"$CONFIG_DIR"/"${item}_backup"
   fi
-
   dotfiles_echo "-> Linking $DOTFILES_DIR/$item to $CONFIG_DIR/$item..."
   ln -nfs "$DOTFILES_DIR"/"$item" "$CONFIG_DIR"/"$item"
 done
 
+if [ -e "$CONFIG_DIR"/starship.toml ]; then
+  dotfiles_echo "starship.toml exists."
+  if [ -L "$CONFIG_DIR"/starship.toml ]; then
+    dotfiles_echo "Symbolic link detected. Removing..."
+    rm -v "$CONFIG_DIR"/starship.toml
+  else
+    dotfiles_echo "Backing up..."
+    mv -v "$CONFIG_DIR"/starship.toml "$CONFIG_DIR"/starship_backup.toml
+  fi
+fi
 dotfiles_echo "-> Linking $DOTFILES_DIR/starship.toml to $CONFIG_DIR/starship.toml..."
 ln -nfs "$DOTFILES_DIR"/starship.toml "$CONFIG_DIR"/starship.toml
 
