dfb46e89d3c2e34e14d2d0843acb6357dd211c2f
Joshua Steele <joshukraine@gmail.com>
2020-03-18 16:35:48 +0200

    Add bb function for smart Brewfile bundling.

---
+++ omf/functions/bb.fish
@@ -0,0 +1,16 @@
+function bb
+    set machine (hostname)
+
+    if [ -d $DOTFILES/machines/$machine ]
+        set brewfile_path $DOTFILES/machines/$machine/Brewfile
+        echo "-> Bundling Brewfile located at $brewfile_path"
+        sleep 2
+        brew bundle --file $brewfile_path
+    else if [ -e $HOME/Brewfile ]
+        echo "-> Bundling Brewfile located at $HOME/Brewfile"
+        sleep 2
+        brew bundle --no-lock --file $HOME/Brewfile
+    else
+        echo "Brewfile not found."
+    end
+end
