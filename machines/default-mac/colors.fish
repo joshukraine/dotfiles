e7a380ec374873df5a57bdfa52482d9809bb90ca
Joshua Steele <joshukraine@gmail.com>
2020-07-24 09:47:06 +0300

    Add fish colors to remaining machine profiles.

---
+++ machines/default-mac/colors.fish
@@ -0,0 +1,9 @@
+set -U fish_color_command brwhite
+set -U fish_color_param brblue
+set -U fish_color_end brgreen
+set -U fish_color_quote yellow
+set -U fish_color_redirection magenta
+set -U fish_color_error red
+set -U fish_color_comment brblack
+set -U fish_color_autosuggestion brblack
+# set -U fish_color_autosuggestion 504945 # Use this for Gruvbox
