13b0ca90521f07e2b9df2d95d3616188abbc105b
Joshua Steele <joshukraine@gmail.com>
2020-08-06 11:05:01 +0300

    Improve dsx function.
    * Use fd instead of find.
    * Provide more useful output.

--- fish/functions/dsx.fish
+++ fish/functions/dsx.fish
@@ -1,3 +1,10 @@
-function dsx
-    find . -name "*.DS_Store" -type f -delete
+function dsx -d "Recursively remove all .DS_Store files"
+    set count (fd -HI '^\.DS_Store$' --type f | wc -l)
+
+    if test ! $count -eq 0
+        fd -HI '^\.DS_Store$' --type f -X rm
+        echo "$count .DS_Store files removed."
+    else
+        echo "No .DS_Store files were found."
+    end
 end
