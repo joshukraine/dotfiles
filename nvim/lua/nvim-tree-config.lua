fd87183130ec7ca0b3b845adbfa181ced8131622
Joshua Steele <joshuasteele@hey.com>
2022-11-15 14:16:17 +0100

    Add actions config

--- nvim/lua/nvim-tree-config.lua
+++ nvim/lua/nvim-tree-config.lua
@@ -27,4 +27,10 @@ require("nvim-tree").setup({
   filters = {
     dotfiles = true,
   },
+  actions = {
+    open_file = {
+      quit_on_open = true,
+      resize_window = true,
+    },
+  },
 })
