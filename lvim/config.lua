dc0e221087b4fcf82f494937339b039b5335e67f
Joshua Steele <joshuasteele@hey.com>
2022-11-23 17:02:21 +0100

    Enable automatic lsp installation

--- lvim/config.lua
+++ lvim/config.lua
@@ -73,7 +73,7 @@ lvim.builtin.treesitter.highlight.enable = true
 -- }
 
 -- ---@usage disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
+lvim.lsp.installer.setup.automatic_installation = true
 
 -- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
