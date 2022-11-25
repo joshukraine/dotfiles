af8a62e9a0f5662fc1fa1ab41f1809692d36ff3b
Joshua Steele <joshuasteele@hey.com>
2022-11-25 14:35:37 +0100

    Refactor

--- lvim/lua/user/functions.lua
+++ lvim/lua/user/functions.lua
@@ -8,15 +8,8 @@ end
 
 function M.toggle_colorcolumn()
   local value = vim.api.nvim_get_option_value("colorcolumn", {})
-
-  if value == "81" then
-    value = "0"
-  else
-    value = "81"
-  end
-
+  if value == "81" then value = "0" else value = "81" end
   vim.opt.colorcolumn = value
-
   vim.notify("colorcolumn" .. " set to " .. tostring(value))
 end
 
