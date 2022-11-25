8e369333b3179520bc833326203d443010c13722
Joshua Steele <joshuasteele@hey.com>
2022-11-25 12:44:32 +0100

    Update options file

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -1,21 +1,34 @@
-lvim.log.level = "warn"
-lvim.format_on_save.enabled = false
-lvim.colorscheme = "lunar"
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+local options = {
+  backup = false,
+  clipboard = "unnamedplus",
+  cmdheight = 1,
+  cursorcolumn = false,
+  cursorline = true,
+  encoding = "utf-8",
+  expandtab = true,
+  hidden = true,
+  ignorecase = true,
+  lazyredraw = false,
+  number = true,
+  regexpengine = 1, -- Force vim to use older regex engine - https://stackoverflow.com/a/16920294/655204
+  relativenumber = true,
+  shiftwidth = 2,
+  showcmd = true,
+  showmode = true,
+  smartcase = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  tabstop = 2,
+  wrap = false,
+  writebackup = false,
+  smarttab = true,
+  shell = "/bin/bash -i", -- Needed since I use fish as main shell
+}
 
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-lvim.builtin.terminal.direction = "vertical"
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
-vim.opt.relativenumber = true
-
-vim.cmd [[set iskeyword+=-]]
+-- vim.cmd [[set iskeyword+=-]]
+vim.cmd [[set list listchars=tab:»·,trail:·,extends:>,precedes:<,nbsp:+]]
