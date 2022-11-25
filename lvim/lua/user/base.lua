be73e6f6d2beecc1626e535f06aa48864f2828ed
Joshua Steele <joshuasteele@hey.com>
2022-11-25 12:45:26 +0100

    Extract base lvim configs

---
+++ lvim/lua/user/base.lua
@@ -0,0 +1,28 @@
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.terminal.active = false
+lvim.builtin.treesitter.highlight.enable = true
+lvim.colorscheme = "lunar"
+lvim.format_on_save.enabled = false
+lvim.log.level = "warn"
+lvim.lsp.installer.setup.automatic_installation = true
+
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "css",
+  "fish",
+  "html",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "ruby",
+  "rust",
+  "toml",
+  "tsx",
+  "typescript",
+  "vue",
+  "yaml",
+}
