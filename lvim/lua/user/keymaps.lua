a6906e8905449c53a350b353503c0b8f3ca383f2
Joshua Steele <joshuasteele@hey.com>
2022-11-25 13:11:11 +0100

    Extract whichkey configs

--- lvim/lua/user/keymaps.lua
+++ lvim/lua/user/keymaps.lua
@@ -21,28 +21,10 @@ lvim.keys.normal_mode["<Down>"] = ":resize -2<CR>"
 lvim.keys.normal_mode["<Left>"] = ":vertical resize +2<CR>"
 lvim.keys.normal_mode["<Right>"] = ":vertical resize -2<CR>"
 
-lvim.builtin.which_key.mappings["-"] = {
-  ":wincmd _<CR>:wincmd |<CR>", "Zoom Window In"
-}
-
-lvim.builtin.which_key.mappings["="] = {
-  ":wincmd =<CR>", "Zoom Window Out"
-}
-
 -- Numeric leaders
 lvim.keys.normal_mode["<leader>1"] = ":set wrap<CR>"
 lvim.keys.normal_mode["<leader>2"] = ":set nowrap<CR>"
 lvim.keys.normal_mode["<leader>4"] = "<c-^>"
-
-lvim.builtin.which_key.mappings["t"] = {
-  name = "Diagnostics",
-  t = { "<cmd>TroubleToggle<cr>", "trouble" },
-  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
-  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
-  q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
-  l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
-  r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
-}
 
 -- Obsession
 lvim.keys.normal_mode["<leader>ob"] = ":Obsession<CR>"
---
+++ lvim/lua/user/whichkey.lua
@@ -0,0 +1,17 @@
+lvim.builtin.which_key.mappings["t"] = {
+  name = "Diagnostics",
+  t = { "<cmd>TroubleToggle<cr>", "trouble" },
+  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+  q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+  l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+  r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+}
+
+lvim.builtin.which_key.mappings["-"] = {
+  ":wincmd _<CR>:wincmd |<CR>", "Zoom Window In"
+}
+
+lvim.builtin.which_key.mappings["="] = {
+  ":wincmd =<CR>", "Zoom Window Out"
+}
