0e98d0dcd62b47daf90158cd9921ca2e34461a9f
Joshua Steele <joshuasteele@hey.com>
2022-11-26 18:40:00 +0100

    Update highlights

--- lvim/after/plugin/highlight.lua
+++ lvim/after/plugin/highlight.lua
@@ -1,4 +1,5 @@
-vim.api.nvim_set_hl(0, 'IlluminatedWord', { link = "CursorLine" })
-vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { link = "CursorLine" })
-vim.api.nvim_set_hl(0, 'IlluminatedWordText', { link = "CursorLine" })
-vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { link = "CursorLine" })
+-- https://github.com/RRethy/vim-illuminate#highlight-groups
+vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "CursorLine" })
+vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "CursorLine" })
+vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "CursorLine" })
+vim.api.nvim_set_hl(0, "ColorColumn", { link = "DiffDelete" })
