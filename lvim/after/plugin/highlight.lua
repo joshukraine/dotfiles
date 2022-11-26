71a104ed04fec823fbe2ec6818a0ba139a5d3954
Joshua Steele <joshuasteele@hey.com>
2022-11-26 15:09:59 +0100

    Add highlights file

---
+++ lvim/after/plugin/highlight.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_set_hl(0, 'IlluminatedWord', { link = "CursorLine" })
+vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { link = "CursorLine" })
+vim.api.nvim_set_hl(0, 'IlluminatedWordText', { link = "CursorLine" })
+vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { link = "CursorLine" })
