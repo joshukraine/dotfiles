c2e5a9c52e95fe68b69853bf3374a2538570ad60
Joshua Steele <joshuasteele@hey.com>
2022-12-01 00:10:35 +0100

    Switch to tokyonight colorscheme

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -1,5 +1,4 @@
 lvim.plugins = {
-  { "arcticicestudio/nord-vim" }, -- Vim colorscheme based on the Nord color palette | https://github.com/arcticicestudio/nord-vim
   { "vim-ruby/vim-ruby" }, -- Vim/Ruby Configuration Files | https://github.com/vim-ruby/vim-ruby
   { "kana/vim-textobj-user" }, -- Create your own text objects | https://github.com/kana/vim-textobj-user
   { "nelstrom/vim-textobj-rubyblock" }, -- A custom text object for selecting ruby blocks | https://github.com/nelstrom/vim-textobj-rubyblock
