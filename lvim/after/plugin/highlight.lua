8f55c97584ea2df0e5658d5c889b5c0ec983b77a
Joshua Steele <joshuasteele@hey.com>
2022-12-10 09:23:34 +0100

    Tweak GitSigns highlights

--- lvim/after/plugin/highlight.lua
+++ lvim/after/plugin/highlight.lua
@@ -3,4 +3,8 @@ vim.api.nvim_set_hl(0, "IlluminatedWord", { link = "CursorLine" })
 vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "CursorLine" })
 vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "CursorLine" })
 vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "CursorLine" })
+
 vim.api.nvim_set_hl(0, "ColorColumn", { link = "Search" })
+
+vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#98be65" })
+vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#7aa2f7" })
