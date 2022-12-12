0c17b2ea7c8739f2c1f92922821e487d023154fc
Joshua Steele <joshuasteele@hey.com>
2022-12-12 22:37:31 +0100

    Simplify options
    - Don't repeat those already set by LunarVim

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -1,35 +1,8 @@
-local options = {
-  backup = false,
-  clipboard = "unnamedplus",
-  cmdheight = 1,
-  cursorcolumn = false,
-  cursorline = true,
-  encoding = "utf-8",
-  expandtab = true,
-  hidden = true,
-  ignorecase = true,
-  lazyredraw = false,
-  number = true,
-  regexpengine = 1, -- Force vim to use older regex engine - https://stackoverflow.com/a/16920294/655204
-  relativenumber = true,
-  shiftwidth = 2,
-  showcmd = true,
-  showmode = false,
-  smartcase = true,
-  splitbelow = true,
-  splitright = true,
-  swapfile = false,
-  tabstop = 2,
-  wrap = false,
-  writebackup = false,
-  smarttab = true,
-  shell = "/bin/bash -i", -- Needed since I use fish as main shell
-  colorcolumn = "0",
-}
+-- LunarVim default settings
+-- https://github.com/LunarVim/LunarVim/blob/1.2.0/lua/lvim/config/settings.lua
 
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
+vim.opt.cursorcolumn = false
+vim.opt.relativenumber = true
+vim.opt.shell = "/bin/bash -i" -- Needed since I use fish as main shell
 
--- vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set list listchars=tab:»·,trail:·,extends:>,precedes:<,nbsp:+]]
