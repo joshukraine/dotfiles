02c795bfa83dc5eb3daab57a6cff9a743c4011c5
Joshua Steele <joshuasteele@hey.com>
2022-12-14 16:07:34 +0100

    Reorder, add comments

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -1,12 +1,14 @@
 -- LunarVim default settings
 -- https://github.com/LunarVim/LunarVim/blob/1.2.0/lua/lvim/config/settings.lua
+--
+-- See :h option-list for more available options
 
 vim.opt.cursorcolumn = false
-vim.opt.relativenumber = true
-vim.opt.shell = "/bin/bash -i" -- Needed since I use fish as main shell
 vim.opt.list = true
+vim.opt.listchars:append({ extends = "…" })
+vim.opt.listchars:append({ nbsp = "+" })
+vim.opt.listchars:append({ precedes = "…" })
 vim.opt.listchars:append({ tab = "»·" })
 vim.opt.listchars:append({ trail = "·" })
-vim.opt.listchars:append({ extends = "…" })
-vim.opt.listchars:append({ precedes = "…" })
-vim.opt.listchars:append({ nbsp = "+" })
+vim.opt.relativenumber = true
+vim.opt.shell = "/bin/bash -i" -- Needed since I use fish as my main shell
