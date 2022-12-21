2e23f9d600362ffda8d0408786964abca3703404
Joshua Steele <joshuasteele@hey.com>
2022-12-21 16:35:01 +0100

    Move highlights to autocommand

--- lvim/lua/user/autocommands.lua
+++ lvim/lua/user/autocommands.lua
@@ -86,4 +86,20 @@ lvim.autocommands = {
       end,
     },
   },
+  {
+    { "ColorScheme" },
+    {
+      desc = "Set custom highlights",
+      pattern = { "*" },
+      callback = function()
+        vim.api.nvim_set_hl(0, "ColorColumn", { link = "Search" })
+        vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#98be65" })
+        vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#7aa2f7" })
+        vim.api.nvim_set_hl(0, "IlluminatedWord", { link = "CursorLine" })
+        vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "CursorLine" })
+        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "CursorLine" })
+        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "CursorLine" })
+      end,
+    },
+  },
 }
