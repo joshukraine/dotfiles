3df904a6ac60cc63786ac5c09f5dcea5f3443861
Joshua Steele <joshuasteele@hey.com>
2022-12-21 16:34:23 +0100

    Simplify augroup syntax

--- lvim/lua/user/autocommands.lua
+++ lvim/lua/user/autocommands.lua
@@ -11,47 +11,47 @@ create_autocmd("BufWritePost", {
   end,
 })
 
-local yankGrp = create_augroup("YankHighlight", { clear = true })
+create_augroup("YankHighlight", { clear = true })
 create_autocmd("TextYankPost", {
   command = "silent! lua vim.highlight.on_yank()",
-  group = yankGrp,
+  group = "YankHighlight",
 })
 
-local fold_settings = create_augroup("fold_settings", { clear = true })
+create_augroup("fold_settings", { clear = true })
 create_autocmd("FileType", {
-  group = fold_settings,
+  group = "fold_settings",
   pattern = { "json" },
   command = [[ setlocal foldmethod=syntax ]],
 })
 create_autocmd("FileType", {
-  group = fold_settings,
+  group = "fold_settings",
   pattern = { "json" },
   command = [[ normal zR ]],
 })
 
-local file_types = create_augroup("file_types", { clear = true })
+create_augroup("file_types", { clear = true })
 create_autocmd({ "BufEnter", "BufRead", "BufNewFile" }, {
-  group = file_types,
+  group = "file_types",
   pattern = { "*.{prettierrc,stylelintrc,babelrc,eslintrc}" },
   command = [[ set filetype=json ]],
 })
 create_autocmd({ "BufEnter", "BufRead", "BufNewFile" }, {
-  group = file_types,
+  group = "file_types",
   pattern = { "gitconfig" },
   command = [[ set filetype=gitconfig ]],
 })
 create_autocmd({ "BufEnter", "BufRead", "BufNewFile" }, {
-  group = file_types,
+  group = "file_types",
   pattern = { "*.spv" },
   command = [[ set filetype=php ]],
 })
 create_autocmd({ "BufEnter", "BufRead", "BufNewFile" }, {
-  group = file_types,
+  group = "file_types",
   pattern = { "*.mdx" },
   command = [[ set filetype=markdown ]],
 })
 create_autocmd({ "BufEnter", "BufRead", "BufNewFile" }, {
-  group = file_types,
+  group = "file_types",
   pattern = { "{aliases,functions,prompt,tmux,oh-my-zsh,opts}" },
   command = [[ set filetype=zsh ]],
 })
