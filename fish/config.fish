86f4f2dec4e1be753764e92f96125a3e388480fc
Joshua Steele <joshuasteele@hey.com>
2023-02-16 08:47:45 -0600

    Update asdf source (back to git version)

--- fish/config.fish
+++ fish/config.fish
@@ -44,4 +44,4 @@ end
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
 
-source /usr/local/opt/asdf/libexec/asdf.fish
+source ~/.asdf/asdf.fish
