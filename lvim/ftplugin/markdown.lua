5f2e10bb780b5c4052f38d019e80d2e5228053e3
Joshua Steele <joshuasteele@hey.com>
2023-03-14 08:00:16 +0100

    Add mappings for en and em dashes

--- lvim/ftplugin/markdown.lua
+++ lvim/ftplugin/markdown.lua
@@ -9,3 +9,8 @@ vim.opt.complete:append("kspell")
 
 vim.api.nvim_set_hl(0, "markdownItalic", { italic = true, fg = "#bb9af7" })
 vim.api.nvim_set_hl(0, "markdownBold", { bold = true, fg = "#e0af68" })
+
+vim.cmd([[
+  inoremap <buffer> --<space> –<space>
+  inoremap <buffer> ---<space> —<space>
+]])
