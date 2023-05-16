df100fc97c930ec610bb056fc977963356ce100a
Joshua Steele <joshuasteele@hey.com>
2023-05-16 23:09:18 +0200

    Remove machines directory

--- fish/.config/fish/functions/bbc.fish
+++ fish/.config/fish/functions/bbc.fish
@@ -1,10 +1,5 @@
 function bbc
-    if [ -d $DOTFILES/machines/$HOST_NAME ]
-        set brewfile_path $DOTFILES/machines/$HOST_NAME/Brewfile
-        echo "-> Running bundle cleanup dry-run for Brewfile located at $brewfile_path"
-        sleep 2
-        brew bundle cleanup --file $brewfile_path
-    else if [ -e $HOME/Brewfile ]
+    if [ -e $HOME/Brewfile ]
         echo "-> Running bundle cleanup dry-run for Brewfile located at $HOME/Brewfile"
         sleep 2
         brew bundle cleanup --file $HOME/Brewfile
