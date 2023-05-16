65175846d178edb499f0dc19c686560a3182ab18
Joshua Steele <joshuasteele@hey.com>
2023-05-16 18:09:48 +0200

    Update zap config

--- zsh/zap.zsh
+++ zsh/zap.zsh
@@ -1,8 +1,8 @@
-# Created by Zap installer
+#https://github.com/zap-zsh/zap
+
 [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
 plug "zsh-users/zsh-autosuggestions"
 plug "zap-zsh/supercharge"
-plug "zap-zsh/zap-prompt"
 plug "zsh-users/zsh-syntax-highlighting"
 
 # Load and initialise completion system
