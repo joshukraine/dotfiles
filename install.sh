caa802edf40e788966fd358880bcb7d6a5fb4f44
Joshua Steele <joshuasteele@hey.com>
2023-05-17 23:52:44 +0200

    Update install file
    - Now uses stow!

--- install.sh
+++ install.sh
@@ -3,228 +3,90 @@
 ################################################################################
 # install
 #
-# This script symlinks dotfiles into place in the home and config directories
+# This script uses GNU Stow to symlink files and directories into place.
 ################################################################################
 
 dotfiles_echo() {
-  local fmt="$1"; shift
+    local fmt="$1"; shift
 
-  # shellcheck disable=SC2059
-  printf "\\n[DOTFILES] ${fmt}\\n" "$@"
+    # shellcheck disable=SC2059
+    printf "\\n[DOTFILES] ${fmt}\\n" "$@"
 }
 
-dotfiles_backup() {
-  if ! command -v gcp >/dev/null || ! command -v gdate >/dev/null; then
-    dotfiles_echo "GNU cp and date commands are required. Please install via Homebrew coreutils: brew install coreutils"
+if ! command -v stow >/dev/null; then
+    dotfiles_echo "GNU Stow is required but was not found. Try:"
+    dotfiles_echo "brew install stow"
+    dotfiles_echo "Exiting..."
     exit 1
-  elif [ -d "$1" ]; then
-    mv -v "$1" "${1}_bak_$(gdate +"%Y%m%d%3N")"
-  else
-    gcp -f --backup=numbered "$1" "$1"
-  fi
-}
-
-if ! command -v lvim >/dev/null; then
-  dotfiles_echo "LunarVim is required but was not found. Please install LunarVim and then run this script again."
-  dotfiles_echo "https://www.lunarvim.org/docs/installation"
-  dotfiles_echo "Exiting..."
-  exit 1
 fi
+
+sudo -v
 
 set -e # Terminate script if anything exits with a non-zero value
 
+dotfiles_echo "Installing dotfiles..."
+
 if [ -z "$DOTFILES" ]; then
-  export DOTFILES="${HOME}/dotfiles"
+    export DOTFILES="${HOME}/dotfiles"
 fi
 
-if [ -z "$HOST_NAME" ]; then
-  HOST_NAME=$(scutil --get HostName)
-  export HOST_NAME
-fi
+dotfiles_echo "Setting HostName..."
 
-if [ ! -d "${DOTFILES}/machines/${HOST_NAME}" ]; then
-  mkdir "${DOTFILES}/machines/${HOST_NAME}"
-  cp "$DOTFILES"/machines/default-mac/* "${DOTFILES}/machines/${HOST_NAME}/"
-fi
+COMPUTER_NAME=$(scutil --get ComputerName)
+LOCAL_HOST_NAME=$(scutil --get LocalHostName)
+
+sudo scutil --set HostName "$LOCAL_HOST_NAME"
+HOST_NAME=$(scutil --get HostName)
+
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server.plist NetBIOSName -string "$HOST_NAME"
+export HOST_NAME
+
+printf "ComputerName:  ==> [%s]\\n" "$COMPUTER_NAME"
+printf "LocalHostName: ==> [%s]\\n" "$LOCAL_HOST_NAME"
+printf "HostName:      ==> [%s]\\n" "$HOST_NAME"
 
 if [ -z "$XDG_CONFIG_HOME" ]; then
-  if [ ! -d "${HOME}/.config" ]; then
-    mkdir "${HOME}/.config"
-  fi
-  export XDG_CONFIG_HOME="${HOME}/.config"
+    dotfiles_echo "Setting up ~/.config directory..."
+    if [ ! -d "${HOME}/.config" ]; then
+        mkdir "${HOME}/.config"
+    fi
+    export XDG_CONFIG_HOME="${HOME}/.config"
 fi
 
-FISH_DIR="${XDG_CONFIG_HOME}/fish"
+dotfiles_echo "Checking your system architecture..."
 
-if [ ! -d "$FISH_DIR" ]; then
-  mkdir "$FISH_DIR"
+osname=$(uname)
+
+if [ "$osname" != "Darwin" ]; then
+    bootstrap_echo "Oops, it looks like you're using a non-Apple system. Sorry, this script
+    only supports macOS. Exiting..."
+    exit 1
 fi
 
 arch="$(uname -m)"
 
 if [ "$arch" == "arm64" ]; then
-  HOMEBREW_PATH="/opt/homebrew/bin"
+    dotfiles_echo "You're on Apple Silicon! Setting HOMEBREW_PREFIX to /opt/homebrew..."
+    HOMEBREW_PREFIX="/opt/homebrew"
 else
-  HOMEBREW_PATH="/usr/local/bin"
+    dotfiles_echo "You're on an Intel Mac! Setting HOMEBREW_PREFIX to /usr/local..."
+    HOMEBREW_PREFIX="/usr/local"
 fi
 
-# Ensure Yarn is available in PATH for when Neovim runs plugin installation.
-# https://github.com/yarnpkg/website/blob/96485d6901f1545a72f413e8df6a6851dece4d75/install.sh#L81
-dotfiles_echo "Adding Yarn to PATH..."
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+dotfiles_echo "-> Setting up symlinks with GNU Stow..."
 
-# Also make sure we have Node, needed by Yarn.
-dotfiles_echo "Adding asdf (Node/Ruby) to PATH..."
-export PATH="$HOME/.asdf/shims:$HOME/.asdf/bin:$PATH"
+cd "$DOTFILES"/ # stow needs to run from inside dotfiles dir
 
-dotfiles_echo "Checking PATH..."
-echo "$PATH"
-
-dotfiles_echo "Do we have Yarn? --> $(command -v yarn)"
-dotfiles_echo "Do we have Node? --> $(command -v node)"
-
-home_files=(
-"asdfrc"
-"default-gems"
-"default-npm-packages"
-"gemrc"
-"gitconfig"
-"gitignore_global"
-"gitmessage"
-# "gitsh_completions"
-"hushlogin"
-"irbrc"
-"npmrc"
-"pryrc"
-"rubocop.yml"
-"tmux.conf"
-"tool-versions"
-"zshrc"
-)
-
-config_dirs=(
-"lvim"
-"fish"
-"ranger"
-"yamllint"
-)
-
-config_files=(
-"alacritty.yml"
-"starship.toml"
-)
-
-# fish_dirs=(
-# "completions"
-# "functions"
-# )
-
-# fish_files=(
-# "config.fish"
-# "abbreviations.fish"
-# )
-
-dotfiles_echo "Installing dotfiles..."
-
-dotfiles_echo "-> Linking basic dotfiles..."
-for item in "${home_files[@]}"; do
-  if [ -e "${HOME}/.${item}" ]; then
-    dotfiles_echo ".${item} exists."
-    if [ -L "${HOME}/.${item}" ]; then
-      dotfiles_echo "Symbolic link detected. Removing..."
-      rm -v "${HOME}/.${item}"
-    else
-      dotfiles_echo "Backing up..."
-      dotfiles_backup "${HOME}/.${item}"
+for item in *; do
+    if [ -d "$item" ]; then
+        stow "$item"/
     fi
-  fi
-  dotfiles_echo "-> Linking ${DOTFILES}/${item} to ${HOME}/.${item}..."
-  ln -nfs "${DOTFILES}/${item}" "${HOME}/.${item}"
 done
 
-dotfiles_echo "-> Linking Brewfile..."
-if [ -e "${HOME}/Brewfile" ]; then
-  dotfiles_echo "Brewfile exists."
-  if [ -L "${HOME}/Brewfile" ]; then
-    dotfiles_echo "Symbolic link detected. Removing..."
-    rm -v "${HOME}/Brewfile"
-  else
-    dotfiles_echo "Backing up..."
-    dotfiles_backup "${HOME}/Brewfile"
-  fi
+if command -v fish >/dev/null; then
+    dotfiles_echo "-> Initializing fish_user_paths..."
+    command fish -c "set -U fish_user_paths $HOME/.asdf/shims $HOME/.local/bin $HOME/.bin $HOME/.yarn/bin $HOMEBREW_PREFIX/bin"
 fi
-dotfiles_echo "-> Linking ${DOTFILES}/Brewfile to ${HOME}/Brewfile..."
-ln -nfs "${DOTFILES}/Brewfile" "${HOME}/Brewfile"
-
-dotfiles_echo "-> Linking config directories..."
-for item in "${config_dirs[@]}"; do
-  if [ -d "${XDG_CONFIG_HOME}/${item}" ]; then
-    dotfiles_echo "Directory ${item} exists."
-    if [ -L "${XDG_CONFIG_HOME}/${item}" ]; then
-      dotfiles_echo "Symbolic link detected. Removing..."
-      rm -v "${XDG_CONFIG_HOME}/${item}"
-    else
-      dotfiles_echo "Backing up..."
-      dotfiles_backup "${XDG_CONFIG_HOME}/${item}"
-    fi
-  fi
-  dotfiles_echo "-> Linking ${DOTFILES}/${item} to ${XDG_CONFIG_HOME}/${item}..."
-  ln -nfs "${DOTFILES}/${item}" "${XDG_CONFIG_HOME}/${item}"
-done
-
-dotfiles_echo "-> Linking config files..."
-for item in "${config_files[@]}"; do
-  if [ -e "${XDG_CONFIG_HOME}/${item}" ]; then
-    dotfiles_echo "${item} exists."
-    if [ -L "${XDG_CONFIG_HOME}/${item}" ]; then
-      dotfiles_echo "Symbolic link detected. Removing..."
-      rm -v "${XDG_CONFIG_HOME}/${item}"
-    else
-      dotfiles_echo "Backing up..."
-      dotfiles_backup "${XDG_CONFIG_HOME}/${item}"
-    fi
-  fi
-  dotfiles_echo "-> Linking ${DOTFILES}/machines/${HOST_NAME}/${item} to ${XDG_CONFIG_HOME}/${item}..."
-  ln -nfs "${DOTFILES}/machines/${HOST_NAME}/${item}" "${XDG_CONFIG_HOME}/$item"
-done
-
-# dotfiles_echo "-> Linking Fish config directories..."
-# for item in "${fish_dirs[@]}"; do
-#   if [ -d "${FISH_DIR}/${item}" ]; then
-#     dotfiles_echo "Directory ${item} exists."
-#     if [ -L "${FISH_DIR}/${item}" ]; then
-#       dotfiles_echo "Symbolic link detected. Removing..."
-#       rm -v "${FISH_DIR}/${item}"
-#     else
-#       dotfiles_echo "Backing up..."
-#       dotfiles_backup "${FISH_DIR}/${item}"
-#     fi
-#   fi
-#   dotfiles_echo "-> Linking ${DOTFILES}/fish/${item} to ${FISH_DIR}/${item}..."
-#   ln -nfs "${DOTFILES}/fish/${item}" "${FISH_DIR}/${item}"
-# done
-
-# dotfiles_echo "-> Linking Fish config files..."
-# for item in "${fish_files[@]}"; do
-#   if [ -e "${FISH_DIR}/${item}" ]; then
-#     dotfiles_echo "${item} exists."
-#     if [ -L "${FISH_DIR}/${item}" ]; then
-#       dotfiles_echo "Symbolic link detected. Removing..."
-#       rm -v "${FISH_DIR}/${item}"
-#     else
-#       dotfiles_echo "Backing up..."
-#       dotfiles_backup "${FISH_DIR}/${item}"
-#     fi
-#   fi
-#   dotfiles_echo "-> Linking ${DOTFILES}/fish/${item} to ${FISH_DIR}/${item}..."
-#   ln -nfs "${DOTFILES}/fish/${item}" "${FISH_DIR}/${item}"
-# done
-
-# dotfiles_echo "-> Installing vim-plug plugins..."
-# nvim --headless +PlugInstall +qall
-
-dotfiles_echo "-> Initializing fish_user_paths..."
-command fish -c "set -U fish_user_paths $HOME/.asdf/shims $HOME/.local/bin $HOME/bin $HOME/.yarn/bin $HOMEBREW_PATH"
 
 dotfiles_echo "-> Installing custom terminfo entries..."
 tic -x "${DOTFILES}/terminfo/tmux-256color.terminfo"
@@ -232,7 +94,3 @@ tic -x "${DOTFILES}/terminfo/xterm-256color-italic.terminfo"
 sudo tic -xe alacritty,alacritty-direct "${DOTFILES}/terminfo/alacritty.info"
 
 dotfiles_echo "Dotfiles installation complete!"
-
-dotfiles_echo "Post-install recommendations:"
-dotfiles_echo "- Complete Brew Bundle installation with 'brew bundle install'"
-dotfiles_echo "- After launching Neovim, run :checkhealth and resolve any errors/warnings."
