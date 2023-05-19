a486e7471f2cb3cce2992c45eca211f3fefe822a
Joshua Steele <joshuasteele@hey.com>
2023-05-19 16:37:57 +0200

    Update install script (now setup)

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 ################################################################################
-# install
+# setup.sh
 #
 # This script uses GNU Stow to symlink files and directories into place.
+# It can be run safely multiple times on the same machine. (idempotency)
 ################################################################################
 
 dotfiles_echo() {
@@ -13,6 +14,23 @@ dotfiles_echo() {
     printf "\\n[DOTFILES] ${fmt}\\n" "$@"
 }
 
+backup_stow_conflict() {
+    dotfiles_echo "Conflict detected: ${1}"
+    dotfiles_echo "Backing up..."
+    local BACKUP_SUFFIX
+    BACKUP_SUFFIX="$(date +%Y-%m-%d)_$(date +%s)"
+    mv -v "$1" "${1}_${BACKUP_SUFFIX}"
+    dotfiles_echo "Moved $1 to ${1}_${BACKUP_SUFFIX}."
+}
+
+osname=$(uname)
+
+if [ "$osname" != "Darwin" ]; then
+    bootstrap_echo "Oops, it looks like you're using a non-Apple system. Sorry, this script
+    only supports macOS. Exiting..."
+    exit 1
+fi
+
 if ! command -v stow >/dev/null; then
     dotfiles_echo "GNU Stow is required but was not found. Try:"
     dotfiles_echo "brew install stow"
@@ -20,11 +38,11 @@ if ! command -v stow >/dev/null; then
     exit 1
 fi
 
+dotfiles_echo "Initializing dotfiles setup..."
+
 sudo -v
 
 set -e # Terminate script if anything exits with a non-zero value
-
-dotfiles_echo "Installing dotfiles..."
 
 if [ -z "$DOTFILES" ]; then
     export DOTFILES="${HOME}/dotfiles"
@@ -55,23 +73,43 @@ fi
 
 dotfiles_echo "Checking your system architecture..."
 
-osname=$(uname)
-
-if [ "$osname" != "Darwin" ]; then
-    bootstrap_echo "Oops, it looks like you're using a non-Apple system. Sorry, this script
-    only supports macOS. Exiting..."
-    exit 1
-fi
-
 arch="$(uname -m)"
 
 if [ "$arch" == "arm64" ]; then
-    dotfiles_echo "You're on Apple Silicon! Setting HOMEBREW_PREFIX to /opt/homebrew..."
+    dotfiles_echo "-> You're on Apple Silicon! Setting HOMEBREW_PREFIX to /opt/homebrew..."
     HOMEBREW_PREFIX="/opt/homebrew"
 else
-    dotfiles_echo "You're on an Intel Mac! Setting HOMEBREW_PREFIX to /usr/local..."
+    dotfiles_echo "-> You're on an Intel Mac! Setting HOMEBREW_PREFIX to /usr/local..."
     HOMEBREW_PREFIX="/usr/local"
 fi
+
+dotfiles_echo "Checking for potential stow conflicts..."
+
+stow_conflicts=(
+".config/lvim"
+".config/zsh"
+".local/bin/colortest"
+".local/bin/git-brst"
+".local/bin/git-cm"
+".local/bin/git-publish"
+".local/bin/git-uncommit"
+".local/bin/tat"
+".tool-versions"
+".zshrc"
+)
+
+for item in "${stow_conflicts[@]}"; do
+    if [ -e "${HOME}/${item}" ]; then
+        # Potential conflict detected
+        if [ -L "${HOME}/${item}" ]; then
+            # This is a symlink and we can ignore it
+            continue
+        else
+            # This is a file or directory
+            backup_stow_conflict "${HOME}/${item}"
+        fi
+    fi
+done
 
 dotfiles_echo "-> Setting up symlinks with GNU Stow..."
 
@@ -88,4 +126,8 @@ if command -v fish >/dev/null; then
     command fish -c "set -U fish_user_paths $HOME/.asdf/shims $HOME/.local/bin $HOME/.bin $HOME/.yarn/bin $HOMEBREW_PREFIX/bin"
 fi
 
-dotfiles_echo "Dotfiles installation complete!"
+dotfiles_echo "Dotfiles setup complete!"
+dotfiles_echo "Possible next steps:"
+dotfiles_echo "-> Install LunarVim (https://www.lunarvim.org)"
+dotfiles_echo "-> Install Zap (https://www.zapzsh.org)"
+dotfiles_echo "-> Install Homebrew packages (brew bundle install)"
