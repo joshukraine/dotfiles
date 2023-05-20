e1552e2115edfadacfe814e170567bf3c2fc3657
Joshua Steele <joshuasteele@hey.com>
2023-05-20 08:48:54 +0200

    Tweak gwip function

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -80,7 +80,7 @@ function glg() {
 }
 
 function gwip() {
-  git add -A && git rm (git ls-files --deleted) 2>/dev/null && git commit --no-verify -m "--wip--"
+  git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify -m "--wip--"
 }
 
 function path() {
