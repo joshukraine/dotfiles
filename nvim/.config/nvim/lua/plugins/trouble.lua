41b6ff9911576a19599dcc84bff37cb83590e775
Joshua Steele <joshukraine@proton.me>
2023-11-12 00:04:05 +0200

    Add base trouble config

---
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,10 @@
+-- https://github.com/folke/trouble.nvimdev
+-- https://www.lazyvim.org/plugins/editor#troublenvim
+
+return {
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
+}
