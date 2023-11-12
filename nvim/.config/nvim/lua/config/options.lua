48d97f7b11f0011379fc68d7fc08feeb2b60d0e0
Joshua Steele <joshukraine@proton.me>
2023-11-12 18:21:09 +0200

    Add custom listchars

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.listchars:append({ extends = "…" })
+vim.opt.listchars:append({ nbsp = "+" })
+vim.opt.listchars:append({ precedes = "…" })
+vim.opt.listchars:append({ tab = "»·" })
+vim.opt.listchars:append({ trail = "·" })
