e8ef9bfd6d5fd8bb9d85b62583a75ed1368264d5
Joshua Steele <joshukraine@proton.me>
2023-11-12 15:09:40 +0200

    Don't hide dotfiles
    - This also solves the coloration problem

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -10,7 +10,7 @@ return {
       filesystem = {
         filtered_items = {
           visible = false, -- when true, they will just be displayed differently than normal items
-          hide_dotfiles = true,
+          hide_dotfiles = false,
           hide_gitignored = true,
           -- hide_hidden = true, -- only works on Windows for hidden files/directories
           hide_by_name = {
