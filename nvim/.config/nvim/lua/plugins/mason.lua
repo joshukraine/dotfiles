fa0a45337a48bd8da2dfaf287bc1718499b63a1e
Joshua Steele <joshukraine@proton.me>
2023-11-14 01:01:15 +0200

    Add mason essentials list

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "bash-language-server",
+        "css-lsp",
+        "emmet-ls",
+        "erb-lint",
+        "eslint-lsp",
+        "flake8",
+        "html-lsp",
+        "json-lsp",
+        "lua-language-server",
+        "markdownlint",
+        "marksman",
+        "phpactor",
+        "prettier",
+        "shellcheck",
+        "shfmt",
+        "solargraph",
+        "stylua",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "vim-language-server",
+        "vue-language-server",
+        "yaml-language-server",
+      },
+    },
+  },
+}
