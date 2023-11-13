70d94dd462168a978cf805ab22b9e0f2beb6a22e
Joshua Steele <joshukraine@proton.me>
2023-11-13 22:48:40 +0200

    Correct typo

--- nvim/.config/nvim/lua/plugins/supertab.lua
+++ nvim/.config/nvim/lua/plugins/supertab.lua
@@ -14,7 +14,7 @@ return {
     dependencies = {
       "hrsh7th/cmp-emoji",
     },
-    -- @param opts cmp.ConfigSchema
+    ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local has_words_before = function()
         unpack = unpack or table.unpack
