53f2f928f39c194b1a4c1dcade0b492b9ead9bbb
Joshua Steele <joshukraine@proton.me>
2023-11-18 23:53:19 +0200

    Add move keymaps for bufferline

---
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "[b", "<cmd>BufferLineMovePrev<cr>", desc = "Buffer move prev" },
+      { "]b", "<cmd>BufferLineMoveNext<cr>", desc = "Buffer move next" },
+    },
+  },
+}
