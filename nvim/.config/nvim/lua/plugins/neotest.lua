58b22473f0949a4ff7c05c35c88b335a74d5a059
Joshua Steele <joshukraine@proton.me>
2023-11-19 00:14:13 +0200

    Install neo-test

---
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "olimorris/neotest-rspec",
+    },
+    opts = {
+      adapters = {
+        ["neotest-rspec"] = {
+          -- NOTE: By default neotest-rspec uses the system wide rspec gem instead of the one through bundler
+          -- rspec_cmd = function()
+          --   return vim.tbl_flatten({
+          --     "bundle",
+          --     "exec",
+          --     "rspec",
+          --   })
+          -- end,
+        },
+      },
+    },
+  },
+}
