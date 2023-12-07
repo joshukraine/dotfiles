bb10b7cb54ae0673927c9f8ad232afb49f33fe1f
Joshua Steele <joshukraine@proton.me>
2023-12-07 10:55:55 +0200

    Formatting fix

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -34,18 +34,18 @@ return {
         },
         config = {
           header = vim.split(logo, "\n"),
-      -- stylua: ignore
-      center = {
-        { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
-        { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
-        { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
-        { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
-        { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
-        { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
-        { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
-        { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
-        { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
-      },
+          -- stylua: ignore
+          center = {
+            { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
+            { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
+            { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
+            { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
+            { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+            { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+            { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
+            { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
+            { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+          },
           footer = function()
             local stats = require("lazy").stats()
             local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
