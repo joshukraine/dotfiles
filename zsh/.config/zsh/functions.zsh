5085287060d604b8d2a571c72d99fc72e00e1897
Joshua Steele <joshukraine@proton.me>
2024-07-30 12:09:50 -0500

    Add copycwd function

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -44,6 +44,32 @@ function ct() {
   ctags -R --languages=ruby --exclude=.git --exclude=log . $(bundle list --paths)
 }
 
+function copycwd() {
+    echo "Choose how to display the home directory:"
+    echo "1) ~"
+    echo "2) \$HOME"
+    echo "3) /Users/$(whoami)"
+    read -r choice
+
+    case $choice in
+        1)
+            pwd | sed "s|^$HOME|~|" | tr -d '\n' | pbcopy
+            echo "Current path copied to clipboard with ~ as home directory."
+            ;;
+        2)
+            pwd | sed "s|^$HOME|\\\$HOME|" | tr -d '\n' | pbcopy
+            echo "Current path copied to clipboard with \$HOME as home directory."
+            ;;
+        3)
+            pwd | tr -d '\n' | pbcopy
+            echo "Current path copied to clipboard with full path as home directory."
+            ;;
+        *)
+            echo "Invalid choice. Please enter 1, 2, or 3."
+            ;;
+    esac
+}
+
 function dsx() {
   find . -name "*.DS_Store" -type f -delete
 }
