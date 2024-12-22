e83f7532af55b4bc3254b3be8be9596ffc35710c
Joshua Steele <joshukraine@proton.me>
2024-12-22 21:00:08 +0200

    Switch to gruvbox colorscheme

---
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,35 @@
+-- Uncomment the conditional to disable this config.
+-- stylua: ignore
+-- if true then return {} end
+
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = false,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = false, -- invert background for search, diffs, statuslines and errors
+      contrast = "soft", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
+    },
+  },
+}
