7029a910f8febeaa92fa1ac9f125316f284bea08
Joshua Steele <joshukraine@proton.me>
2025-01-09 22:18:08 +0200

    Use nvim-lint to reference global markdown lint config

---
+++ nvim/.config/nvim/lua/plugins/extend-nvim-lint.lua
@@ -0,0 +1,13 @@
+-- Adapted from https://github.com/linkarzu/dotfiles-latest/blob/main/neovim/neobean/lua/plugins/nvim-lint.lua
+
+return {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", os.getenv("HOME") .. "/.markdownlint.yaml", "--" },
+      },
+    },
+  },
+}
