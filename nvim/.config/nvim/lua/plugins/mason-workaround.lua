71807ff2bb09b47fe4a3257eabf90100cce4e39f
Joshua Steele <joshukraine@gmail.com>
2025-05-12 11:17:35 +0300

    Add mason workaround
    - https://github.com/LazyVim/LazyVim/issues/6039
    - https://github.com/LazyVim/LazyVim/pull/6053

---
+++ nvim/.config/nvim/lua/plugins/mason-workaround.lua
@@ -0,0 +1,6 @@
+-- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
